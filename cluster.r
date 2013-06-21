6963e3e754c6390dd18e973c08a31f0aa20e4f78
fingerman <adamov.george@gmail.com>
2013-06-22 00:53:16 +0300

    Update cluster.r

--- cluster.r
+++ cluster.r
@@ -1,7 +1,13 @@
 library(data.table)
 
 dat <- data.table(read.csv('car.data'))
+
+#Georgi:by opening it like this we loose the first observation. I propose 
+#cdata <- data.table(read.csv('car.data', header=FALSE))
+
 cols <- c('buying','maint','doors','people','trunk','safety','class')
+
+
 
 setnames(dat, cols)
 
@@ -12,6 +18,13 @@ print(summary(dat))
 # metricization
 for(v in cols)
   levels(dat[[v]]) <- 1:length(levels(dat[[v]]))
+  
+# Georgi: After metricization some conserns arise:  The realizations are neither ordered or comparable. I mean:
+# "buying": 4-vhigh; 1-high, 3-med, 2-low; the same for "maint"; "safety": 2-low, 3-med, 1-high. Let us consider some
+# unified base structure for the analysis of each one of us. 
+# I think application of PCA or FA should also make sense.
+# ah and sth more - the matrix for cluster anal. should be standarized. I suppose "method = euclidean" takes this into account?
+  
 
 print(dat)
 
